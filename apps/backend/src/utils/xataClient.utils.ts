6a19fe450b60d22508049099ece95c0b40230009
symonmuchemi <simonkenya846@gmail.com>
2024-10-20 22:06:09 +0300

    refactor: deletes all directories to use simpler structure

--- apps/backend/src/utils/xataClient.utils.ts
+++
@@ -1,9 +0,0 @@
-import { XataClient } from "../xata";
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export const xata: XataClient = new XataClient({
-    apiKey: process.env.XATA_API_KEY,
-    branch: process.env.XATA_BRANCH
-})
