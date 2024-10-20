d2b6571d5397e187c24d87a22369e4b4cb6ce957
symonmuchemi <simonkenya846@gmail.com>
2024-10-20 20:10:49 +0300

    refactor: sets up new xata db configurations
    this commits also creates a simple script that fethes the prepolulated data from the User tbale in the new database.

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,1 +1,26 @@
-console.log('Hello from Task Manager');
+import express, { Express, Request, Response } from "express";
+import { XataClient } from "./xata";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port: string | number = process.env.PORT || 3500;
+const xata: XataClient = new XataClient({
+    apiKey: process.env.XATA_API_KEY,
+    branch: process.env.XATA_BRANCH
+});
+
+app.get('/', (req: Request, resp: Response) => {
+    resp.json({ msg: 'Hello' })
+});
+
+app.get('/users', async (req: Request, resp: Response) => {
+    const users = await xata.db.User.getAll();
+
+    resp.json({ users });
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening on port: ${port}`);
+});
