70e81af95d71d320291ecaa2ee1428f9db41ebf9
symonmuchemi <simonkenya846@gmail.com>
2024-10-20 18:57:32 +0300

    update: deletes file contents

--- apps/backend/src/services/authServices.ts
+++
@@ -1,6 +0,0 @@
-import { User } from "../types/user.types";
-import { XataClient } from "../xata";
-import jwt from 'jsonwebtoken';
-
-
-
--- apps/backend/src/utils/xataClient.utils.ts
+++
@@ -1,5 +0,0 @@
-import { XataClient } from "../xata";
-
-export const xata: XataClient = new XataClient({
-    apiKey: process.env.
-})
