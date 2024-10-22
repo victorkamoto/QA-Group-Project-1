0b25e0a70d5a5123aea9263abefd51bc1b7267ce
victorkamoto <victorkamoto@gmail.com>
2024-10-22 14:40:20 +0300

    feat: auth

---
+++ apps/frontend/app/(auth)/signup/page.tsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+
+import { cn } from "../../../lib/utils";
+import { buttonVariants } from "../../../components/ui/button";
+import { SignupForm } from "../../../components/signup";
+import Image from "next/image";
+import { Icons } from "@/components/icons";
+
+export const metadata = {
+  title: "Create an account",
+  description: "Create an account to get started.",
+};
+
+export default function RegisterPage() {
+  return (
+    <div className="container flex h-screen w-screen flex-col items-center justify-center">
+      <Link
+        href="/"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "absolute left-4 top-4 md:left-8 md:top-8",
+        )}
+      >
+        <>
+          <Icons.chevronLeft className="mr-2 h-4 w-4" />
+          Home
+        </>
+      </Link>
+      <Link
+        href="/signin"
+        className={cn(
+          buttonVariants({ variant: "default" }),
+          "absolute right-4 top-4 md:right-8 md:top-8",
+        )}
+      >
+        Login
+      </Link>
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="flex flex-col space-y-2 text-center">
+          {/* <Icons.logo className="mx-auto h-6 w-6" /> */}
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Create an account
+          </h1>
+          <p className="text-sm text-muted-foreground">
+            Enter your credentials below to create your account
+          </p>
+        </div>
+        <SignupForm />
+      </div>
+    </div>
+  );
+}
