6ea5557c648cc1f381e8cef97e87e1e93ffd0f2b
symonmuchemi <simonkenya846@gmail.com>
2024-10-23 22:00:19 +0300

    refactor: Add convertXataRecordToTask function and getTaskDifferences function
    Add the convertXataRecordToTask function to convert a TaskRecord object to a Task object. Also, add the getTaskDifferences function to compare two TaskRecord objects and return the differences as a Partial<Task> object.

--- apps/backend/src/utils/index.ts
+++ apps/backend/src/utils/index.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from "express";
+import { Task, isValidStatus } from "../types/task.types";
+import { TaskRecord } from "../xata";
 
 /**
  * A higher-order function that wraps an asynchronous route handler function
@@ -10,6 +12,48 @@ import { Request, Response, NextFunction } from "express";
  */
 export const asyncHandler =
   (fn: (req: Request, res: Response, next: NextFunction) => Promise<any>) =>
-  (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
+    (req: Request, res: Response, next: NextFunction) => {
+      Promise.resolve(fn(req, res, next)).catch(next);
+    };
+
+const convertXataRecordToTask = (record: TaskRecord) => {
+  return {
+    xata_id: record.xata_id,
+    description: record.description,
+    dueDate: new Date(record.dueDate),
+    status: record.status as "in-progress" | "completed",
+    projectId: record.projectId,
+    assignedToId: record.assignedToId
+  }
+}
+
+
+
+export function getTaskDifferences(task1: TaskRecord, task2: TaskRecord): Partial<Task> {
+  const differences: Partial<Task> = {};
+
+  const originalTask = convertXataRecordToTask(task1);
+  const updatedTask = convertXataRecordToTask(task2);
+
+  // Loop through each key in the updatedTask
+  (Object.keys(updatedTask) as Array<keyof Task>).forEach(key => {
+    if (key === 'dueDate') {
+      // Handle Date comparison
+      if (originalTask[key].getTime() !== updatedTask[key].getTime()) {
+        differences[key] = updatedTask[key];
+      }
+    } else if (key === 'status') {
+      // Handle status comparison
+      if (originalTask[key] !== updatedTask[key]) {
+        differences[key] = updatedTask[key];
+      }
+    } else {
+      // Handle other string comparisons
+      if (originalTask[key] !== updatedTask[key]) {
+        differences[key] = updatedTask[key];
+      }
+    }
+  });
+
+  return differences;
+}
