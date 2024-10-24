5ef8ab72a7f642f2d57a1c846624074fb50aaa85
symonmuchemi <simonkenya846@gmail.com>
2024-10-24 15:10:51 +0300

    feat: Add getTeamMemberById service and route

--- apps/backend/src/controllers/teamMember.controller.ts
+++ apps/backend/src/controllers/teamMember.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { fetchAllTeamMembers } from "../services/teamMember.services";
+import { fetchAllTeamMembers, fetchTeamMemberById } from "../services/teamMember.services";
 
 
 /**
@@ -14,6 +14,33 @@ import { fetchAllTeamMembers } from "../services/teamMember.services";
 export const getAllTeamMembers = async (req: Request, res: Response): Promise<Response> => {
     try {
         const { code, message, details } = await fetchAllTeamMembers();
+
+        return res.status(code).json({ message, details });
+    } catch (error: any) {
+        return res.status(500).json({ error: error.toString() });
+    }
+}
+
+
+/**
+ * Retrieves a team member by their ID.
+ *
+ * @param req - The request object containing the team member ID in the query parameters.
+ * @param res - The response object used to send back the HTTP response.
+ * @returns A promise that resolves to an HTTP response containing the team member details or an error message.
+ *
+ * @throws Will return a 400 status code if the `memberId` query parameter is missing.
+ * @throws Will return a 500 status code if an unexpected error occurs during the process.
+ */
+export const getTeamMemberById = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        const teamMemberId = req.query.memberId?.toString();
+
+        if (!teamMemberId) {
+            return res.status(400).json({ message: 'memberId is required!' });
+        }
+
+        const { code, message, details } = await fetchTeamMemberById(teamMemberId);
 
         return res.status(code).json({ message, details });
     } catch (error: any) {
--- apps/backend/src/services/teamMember.services.ts
+++ apps/backend/src/services/teamMember.services.ts
@@ -35,3 +35,37 @@ export const fetchAllTeamMembers = async () => {
     }
 }
 
+/**
+ * Fetches a team member by their User ID from the database.
+ *
+ * @param id - The unique identifier of the team member.
+ * @returns An object containing the status code, message, and details.
+ * - If the team member is found, returns a 200 status code with the team member details.
+ * - If the team member is not found, returns a 404 status code with a not found message.
+ * - If an error occurs, returns a 500 status code with the error message.
+ */
+export const fetchTeamMemberById = async (id: string) => {
+    try {
+        const teamMember = await xata.db.TeamMember.filter({ userId: id }).getFirst();
+
+        if (!teamMember) {
+            return {
+                code: 404,
+                message: 'Team member not found!',
+                details: `Team member with id ${id} not found!`
+            }
+        }
+
+        return {
+            code: 200,
+            message: 'Team member found!',
+            details: teamMember
+        }
+    } catch (error: any) {
+        return {
+            code: 500,
+            message: 'Error fetching team member!',
+            details: error.toString()
+        }
+    }
+}
