15990b1f8e1eec7dfcec844301708e010eaa9776
victorkamoto <victorkamoto@gmail.com>
2024-10-31 21:14:53 +0300

    fix: backend put/patch & select foreign fields on update

--- apps/backend/src/routers/task.router.ts
+++ apps/backend/src/routers/task.router.ts
@@ -2,23 +2,21 @@ import { Router } from "express";
 import { createTaskSchema } from "../middleware/validators/task.validators";
 import { asyncHandler } from "../utils";
 import {
-    create,
-    getTasks,
-    getTaskById,
-    update,
-    deleteTask,
-    updateStatus,
-    getTasksByUserId
+  create,
+  getTasks,
+  getTaskById,
+  update,
+  deleteTask,
+  getTasksByUserId,
 } from "../controllers/task.controller";
 
 const taskRouter = Router();
 
-taskRouter.post('/', createTaskSchema, asyncHandler(create));
-taskRouter.get('/', asyncHandler(getTasks));
-taskRouter.get('/:id', asyncHandler(getTaskById));
-taskRouter.get('/user/:id', asyncHandler(getTasksByUserId));
-taskRouter.put('/:id', asyncHandler(update));
-taskRouter.patch('/:id', asyncHandler(updateStatus));
-taskRouter.delete('/:id', asyncHandler(deleteTask));
+taskRouter.post("/", createTaskSchema, asyncHandler(create));
+taskRouter.get("/", asyncHandler(getTasks));
+taskRouter.get("/:id", asyncHandler(getTaskById));
+taskRouter.get("/user/:id", asyncHandler(getTasksByUserId));
+taskRouter.patch("/:id", asyncHandler(update));
+taskRouter.delete("/:id", asyncHandler(deleteTask));
 
 export default taskRouter;
