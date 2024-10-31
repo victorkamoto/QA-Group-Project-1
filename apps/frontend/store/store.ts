8aebd8ca9edcdd65ce175b33a22911865989d6c9
victorkamoto <victorkamoto@gmail.com>
2024-10-31 15:18:14 +0300

    chore: redux devtools middleware

--- apps/frontend/store/store.ts
+++ apps/frontend/store/store.ts
@@ -20,6 +20,7 @@ import {
 import { Column, CreateTask, Task, UpdateTask } from "../types/task.types";
 import { createTask, deleteTask, getTasks, updateTask } from "../lib/tasks";
 import { getUser } from "../lib/auth";
+import { devtools } from "zustand/middleware";
 
 interface StoreState {
   //   user: User;
@@ -51,137 +52,139 @@ interface StoreState {
   setKanban: (columns: Column[]) => void;
 }
 
-export const store = create<StoreState>()((set) => ({
-  getUser: async (id) => {
-    const { status, data } = await getUser(id);
-    return { status, data };
-  },
-
-  teams: [],
-  getTeams: async () => {
-    const { data } = await getTeams();
-    set({ teams: [...data] });
-  },
-  joinTeam: async (teamId, userId) => {
-    const { status, data } = await joinTeam(teamId, userId);
-    set((state) => {
-      const teams = state.teams.filter((t) => t.xata_id != teamId);
-      return { teams: [...teams, data] };
-    });
-    return { status };
-  },
-  createTeam: async (team) => {
-    const { status, data } = await createTeam(team);
-    set((state) => {
-      return { teams: [...state.teams, data] };
-    });
-    return { status };
-  },
-  updateTeam: async (id, team) => {
-    const { status, data } = await updateTeam(id, team);
-    set((state) => {
-      const teams = state.teams.filter((t) => t.xata_id != id);
-      return { teams: [...teams, data] };
-    });
-    return { status };
-  },
-  deleteTeam: async (id) => {
-    const { status } = await deleteTeam(id);
-    set((state) => {
-      const teams = state.teams.filter((t) => t.xata_id != id);
-      return { teams: [...teams] };
-    });
-    return { status };
-  },
-  getMembersByTeamId: async (id) => {
-    // TODO: deduplication
-    const { status, data } = await getMembersByTeamId(id);
-    return { status, data };
-  },
-
-  projects: [],
-  getProjects: async () => {
-    const { data } = await getProjects();
-    set({ projects: [...data] });
-  },
-  getProject: async (id) => {
-    const { status, data } = await getProjectById(id);
-    return { status, data };
-  },
-  createProject: async (project) => {
-    const { status, data } = await createProject(project);
-    set((state) => {
-      return { projects: [...state.projects, data] };
-    });
-    return { status };
-  },
-  updateProject: async (id, project) => {
-    const { status, data } = await updateProject(id, project);
-    set((state) => {
-      const projects = state.projects.filter((p) => p.xata_id != id);
-      return { projects: [...projects, data] };
-    });
-    return { status };
-  },
-  deleteProject: async (id) => {
-    const { status } = await deleteProject(id);
-    set((state) => {
-      const projects = state.projects.filter((p) => p.xata_id != id);
-      return { projects: [...projects] };
-    });
-    return { status };
-  },
-
-  kanban: [
-    {
-      id: "backlog",
-      title: "Backlog",
-      tasks: [],
+export const store = create<StoreState>()(
+  devtools((set) => ({
+    getUser: async (id) => {
+      const { status, data } = await getUser(id);
+      return { status, data };
     },
-    {
-      id: "in-progress",
-      title: "In Progress",
-      tasks: [],
-    },
-    {
-      id: "review",
-      title: "Review",
-      tasks: [],
-    },
-    {
-      id: "completed",
-      title: "Completed",
-      tasks: [],
-    },
-  ],
-  setKanban: (columns) => set({ kanban: columns }),
 
-  tasks: [],
-  getTasks: async () => {
-    const { data } = await getTasks();
-    set({ tasks: [...data] });
-  },
-  createTask: async (task) => {
-    const { status, data } = await createTask(task);
-    set((state) => {
-      return { tasks: [...state.tasks, data] };
-    });
-    return { status };
-  },
-  updateTask: async (id, task) => {
-    const { status, data } = await updateTask(id, task);
-    set((state) => {
-      const tasks = state.tasks.filter((t) => t.xata_id != id);
-      return { tasks: [...tasks, data] };
-    });
-    return { status };
-  },
-  deleteTask: async (id) => {
-    const { status } = await deleteTask(id);
-    set((state) => {
-      const tasks = state.tasks.filter((t) => t.xata_id != id);
-      return { tasks: [...tasks] };
-    });
-    return { status };
-  },
-}));
+    teams: [],
+    getTeams: async () => {
+      const { data } = await getTeams();
+      set({ teams: [...data] });
+    },
+    joinTeam: async (teamId, userId) => {
+      const { status, data } = await joinTeam(teamId, userId);
+      set((state) => {
+        const teams = state.teams.filter((t) => t.xata_id != teamId);
+        return { teams: [...teams, data] };
+      });
+      return { status };
+    },
+    createTeam: async (team) => {
+      const { status, data } = await createTeam(team);
+      set((state) => {
+        return { teams: [...state.teams, data] };
+      });
+      return { status };
+    },
+    updateTeam: async (id, team) => {
+      const { status, data } = await updateTeam(id, team);
+      set((state) => {
+        const teams = state.teams.filter((t) => t.xata_id != id);
+        return { teams: [...teams, data] };
+      });
+      return { status };
+    },
+    deleteTeam: async (id) => {
+      const { status } = await deleteTeam(id);
+      set((state) => {
+        const teams = state.teams.filter((t) => t.xata_id != id);
+        return { teams: [...teams] };
+      });
+      return { status };
+    },
+    getMembersByTeamId: async (id) => {
+      // TODO: deduplication
+      const { status, data } = await getMembersByTeamId(id);
+      return { status, data };
+    },
+
+    projects: [],
+    getProjects: async () => {
+      const { data } = await getProjects();
+      set({ projects: [...data] });
+    },
+    getProject: async (id) => {
+      const { status, data } = await getProjectById(id);
+      return { status, data };
+    },
+    createProject: async (project) => {
+      const { status, data } = await createProject(project);
+      set((state) => {
+        return { projects: [...state.projects, data] };
+      });
+      return { status };
+    },
+    updateProject: async (id, project) => {
+      const { status, data } = await updateProject(id, project);
+      set((state) => {
+        const projects = state.projects.filter((p) => p.xata_id != id);
+        return { projects: [...projects, data] };
+      });
+      return { status };
+    },
+    deleteProject: async (id) => {
+      const { status } = await deleteProject(id);
+      set((state) => {
+        const projects = state.projects.filter((p) => p.xata_id != id);
+        return { projects: [...projects] };
+      });
+      return { status };
+    },
+
+    kanban: [
+      {
+        id: "backlog",
+        title: "Backlog",
+        tasks: [],
+      },
+      {
+        id: "in-progress",
+        title: "In Progress",
+        tasks: [],
+      },
+      {
+        id: "review",
+        title: "Review",
+        tasks: [],
+      },
+      {
+        id: "completed",
+        title: "Completed",
+        tasks: [],
+      },
+    ],
+    setKanban: (columns) => set({ kanban: columns }),
+
+    tasks: [],
+    getTasks: async () => {
+      const { data } = await getTasks();
+      set({ tasks: [...data] });
+    },
+    createTask: async (task) => {
+      const { status, data } = await createTask(task);
+      set((state) => {
+        return { tasks: [...state.tasks, data] };
+      });
+      return { status };
+    },
+    updateTask: async (id, task) => {
+      const { status, data } = await updateTask(id, task);
+      set((state) => {
+        const tasks = state.tasks.filter((t) => t.xata_id != id);
+        return { tasks: [...tasks, data] };
+      });
+      return { status };
+    },
+    deleteTask: async (id) => {
+      const { status } = await deleteTask(id);
+      set((state) => {
+        const tasks = state.tasks.filter((t) => t.xata_id != id);
+        return { tasks: [...tasks] };
+      });
+      return { status };
+    },
+  }))
+);
