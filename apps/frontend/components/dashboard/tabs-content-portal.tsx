40e07c20c09e694076a8bca94342b6aec438cce2
victorkamoto <victorkamoto@gmail.com>
2024-10-31 13:25:49 +0300

    chore: task-item skeleton on list-view loading

--- apps/frontend/components/dashboard/tabs-content-portal.tsx
+++ apps/frontend/components/dashboard/tabs-content-portal.tsx
@@ -7,15 +7,23 @@ import { TabsContent } from "../ui/tabs";
 import TaskItem from "../tasks/task-item";
 import { Task } from "../../types/task.types";
 import { store } from "../../store/store";
+import { TaskItemSkeleton } from "../tasks/task-item-skeleton";
 
 export function TabsPortalContent() {
   const [mounted, setMounted] = useState(false);
   const tasks = store((state) => state.tasks);
   const getTasks = store((state) => state.getTasks);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     setMounted(true);
 
-    getTasks();
+    const fetchTasks = async () => {
+      await getTasks();
+      setIsLoading(false);
+    };
+
+    fetchTasks();
   }, []);
 
   if (!mounted) return null;
@@ -23,9 +31,15 @@ export function TabsPortalContent() {
   return createPortal(
     <div>
       <TabsContent value="list">
-        {tasks.map((t) => (
-          <TaskItem task={t} key={t.xata_id} />
-        ))}
+        {isLoading ? (
+          <>
+            <TaskItemSkeleton />
+            <TaskItemSkeleton />
+            <TaskItemSkeleton />
+          </>
+        ) : (
+          tasks.map((t) => <TaskItem task={t} key={t.xata_id} />)
+        )}
       </TabsContent>
       <TabsContent value="kanban">
         <KanbanBoard tasks={tasks} />
